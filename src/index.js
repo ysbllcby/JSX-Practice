7163a1044fa6789980aaba742e5234ac05b2d537
Ma Cubay <ma.cubay@edu.sait.ca>
2023-05-25 21:25:22 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Ysa's hobbies:</h1>
+    <ul>
+      <li>Reading</li>
+      <li>Practicing coding</li>
+      <li>Calling friends</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
